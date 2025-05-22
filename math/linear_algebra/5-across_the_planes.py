d5b7732173bdb7663180c63e4dbe206f8888f0a2
Q-Reine <q.wihogora@alustudent.com>
2025-05-22 14:50:07 +0000

    Adds two matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """  Adds two matrices element-wise """
 
+
 def add_matrices2D(mat1, mat2):
     """ returns new matrix, the sum of two 2D matrices added element-wise """
     if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
