073bf96a3e02da670806d19dd24d8af43a354a57
Q-Reine <q.wihogora@alustudent.com>
2025-05-22 16:02:49 +0000

    Function that adds two matrices

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -5,9 +5,16 @@
 def add_matrices(mat1, mat2):
     """ returns list of integers representing dimensions of given matrix """
 
+
     if isinstance(mat1, list) and isinstance(mat2, list):
         if len(mat1) == len(mat2):
-            return [add_matrices(a, b) for a, b in zip(mat1, mat2)]
+            result = []
+            for a, b in zip(mat1, mat2):
+                sum_result = add_matrices(a, b)
+                if sum_result is None:
+                    return None
+                result.append(sum_result)
+            return result
     elif isinstance(mat1, (int, float)) and isinstance(mat2, (int, float)):
         return mat1 + mat2
     return None
